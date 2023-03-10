3651551167c1575762aa95bf4456b237042896da
hackerzgz <hackerzgz@gmail.com>
2023-03-10 19:01:52 +0800

    feat: export crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,9 @@
 
 mod bot;
 mod message;
+
+pub use bot::{WeComBot, WeComBotBuilder};
+pub use message::Message;
+
+#[cfg(feature = "async_api")]
+pub use bot::{WeComBotAsync, WeComBotAsyncBuilder};
